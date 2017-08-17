b1ed4ff3004ed5e920ec17f3388c1cb9ed2af1f6
RJ Mohammad <rana.mohammad@weightwatchers.com>
2017-08-16 20:48:16 -0400

    added more tests

--- test/search.js
+++ test/search.js
@@ -6,7 +6,7 @@ const CHROMELESS_OPTIONS = {debug: true}
 
 describe('Search Functionality', async function() {
 	it('Can search on homepage and get to SRP', async function () {
-		// this.timeout(10000);
+		this.timeout(10000);
 		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/?locale=national`)
@@ -14,13 +14,14 @@ describe('Search Functionality', async function() {
 			.type('New York, NY\r', 'input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
+			console.log(URL)
 
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/new-york-ny`)
 			await chromeless.end();
 	})
 
 	it('Can search from SRP and get to another SRP', async function () {
-		// this.timeout(10000);
+		this.timeout(10000);
 		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/search/atlanta-ga`)
@@ -35,7 +36,7 @@ describe('Search Functionality', async function() {
 		})
 
 	it('Can search from LDP and get to SRP', async function () {
-		// this.timeout(10000);
+		this.timeout(10000);
 		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/search/binder-ca?property_id=2593658897`)
@@ -50,38 +51,33 @@ describe('Search Functionality', async function() {
 	})
 
 
-	// it('Can search from article and get to SRP', async function () {
-	// 	const chromeless = new Chromeless()
-	// 	console.log('starting...4')
-	// 	return await chromeless
-	// 		.goto(`${process.env.ORIGIN}/articles/essential-design-tips-every-renter-should-know`)
-	// 		.type('West Loop\r', 'input[class="input_m7611f-o_O-navBarInput_1awpofx search-box-placeholder"]')
-	// 		.wait(3000)
-	// 		.evaluate(() => document.URL)
-	// 		.end()
-	// 		.then(res => {
-	// 			console.log('res', res)
-	// 			expect(res).toEqual(`${process.env.ORIGIN}/search/chicago-il/west-loop`)
-	// 			return
-	// 		})
-	// })
+	it('Can search from article and get to SRP', async function () {
+		this.timeout(10000);
+		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		const URL = await chromeless
+			.goto(`${process.env.ORIGIN}/articles/essential-design-tips-every-renter-should-know`)
+			.wait('input[class="input_m7611f-o_O-navBarInput_1awpofx search-box-placeholder"]')
+			.type('West Loop\r', 'input[class="input_m7611f-o_O-navBarInput_1awpofx search-box-placeholder"]')
+			.wait('.srpList_1sc4ubv')
+			.evaluate(() => document.URL)
 
-	// it('Can search using search button', async function () {
-	// 	const chromeless = new Chromeless()
-	// 	console.log('starting...5')
-	// 	return await chromeless
-	// 		.goto(`${process.env.ORIGIN}/?locale=national`)
-	// 		.type('New York, NY', 'input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
-	// 		.click('button[class="searchButton_xb5mu5-o_O-homeSearchButton_1c55jrk"]')
-	// 		.wait(3000)
-	// 		.evaluate(() => document.URL)
-	// 		.end()
-	// 		.then(res => {
-	// 			console.log('res', res)
-	// 			expect(res).toEqual(`${process.env.ORIGIN}/search/new-york-ny`)
-	// 			return
-	// 		})
-	// })
+			expect(URL).toEqual(`${process.env.ORIGIN}/search/chicago-il/west-loop`)
+			await chromeless.end();
+	})
+
+	it('Can search using search button', async function () {
+		this.timeout(10000);
+		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		const URL = await chromeless
+			.goto(`${process.env.ORIGIN}/?locale=national`)
+			.wait('input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
+			.type('New York, NY', 'input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
+			.click('button[class="searchButton_xb5mu5-o_O-homeSearchButton_1c55jrk"]')
+			.wait('.srpList_1sc4ubv')
+			.evaluate(() => document.URL)
+			expect(URL).toEqual(`${process.env.ORIGIN}/search/new-york-ny`)
+			await chromeless.end();
+	})
 
 	// it('Can search for national city via typeahead and get to SRP', async function () {
 	// 	const chromeless = new Chromeless()
