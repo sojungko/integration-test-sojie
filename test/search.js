40e319879e2fc5e0936613a1d99d7a5108425e43
sojungko <sojungko@gmail.com>
2017-08-24 16:51:58 -0400

    use only one instance of chromeless

--- test/search.js
+++ test/search.js
@@ -6,9 +6,9 @@ const CHROMELESS_OPTIONS = {
 }
 
 describe('Search Functionality', async function() {
+	this.timeout(70000);
+	const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 	it('Can search on homepage and get to SRP', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/?locale=national`)
 			.wait('input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
@@ -16,13 +16,12 @@ describe('Search Functionality', async function() {
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
 
-			await chromeless.end();
+			// await chromeless.end();
 			await expect(URL).toEqual(`${process.env.ORIGIN}/search/new-york-ny`)
 	})
 
 	it('Can search from SRP and get to another SRP', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		// const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/search/atlanta-ga`)
 			.wait('input[class="input_m7611f-o_O-navBarInput_1awpofx search-box-placeholder"]')
@@ -30,14 +29,14 @@ describe('Search Functionality', async function() {
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
 
-			await chromeless.end();
+			// await chromeless.end();
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/chicago-il`)
 		
 		})
 
 	it('Can search from LDP and get to SRP', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		// this.timeout(70000);
+		// const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/search/binder-ca?property_id=2593658897`)
 			.wait('input[class="input_m7611f-o_O-navBarInput_1awpofx search-box-placeholder"]')
@@ -45,15 +44,15 @@ describe('Search Functionality', async function() {
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
 
-			await chromeless.end();
+			// await chromeless.end();
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/brooklyn-ny`)
 		
 	})
 
 
 	it('Can search from article and get to SRP', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		// this.timeout(70000);
+		// const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/articles/essential-design-tips-every-renter-should-know`)
 			.wait('input[class="input_m7611f-o_O-navBarInput_1awpofx search-box-placeholder"]')
@@ -61,13 +60,13 @@ describe('Search Functionality', async function() {
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
 
-			await chromeless.end();
+			// await chromeless.end();
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/chicago-il/west-loop`)
 	})
 
 	it('Can search using search button', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		// this.timeout(70000);
+		// const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/?locale=national`)
 			.wait('input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
@@ -75,13 +74,13 @@ describe('Search Functionality', async function() {
 			.click('button[class="searchButton_xb5mu5-o_O-homeSearchButton_1c55jrk"]')
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
-			await chromeless.end();
+			// await chromeless.end();
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/new-york-ny`)
 	})
 
 	it('Can search for national city via typeahead and get to SRP', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		// this.timeout(70000);
+		// const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/?locale=national`)
 			.wait('input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
@@ -92,13 +91,13 @@ describe('Search Functionality', async function() {
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
 
-			await chromeless.end();
+			// await chromeless.end();
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/san-francisco-ca`)
 	})
 
 	it('Can search for featured neighborhood on locale page via typeahead get to SRP', async function () {
-		this.timeout(70000);
-		const chromeless = new Chromeless(CHROMELESS_OPTIONS)
+		// this.timeout(70000);
+		// const chromeless = new Chromeless(CHROMELESS_OPTIONS)
 		const URL = await chromeless
 			.goto(`${process.env.ORIGIN}/?locale=phoenix-az`)
 			.wait('input[class="input_m7611f-o_O-homeInput_1euh3ve search-box-placeholder"]')
@@ -109,7 +108,7 @@ describe('Search Functionality', async function() {
 			.wait('.srpList_1sc4ubv')
 			.evaluate(() => document.URL)
 
-			await chromeless.end();
+			// await chromeless.end();
 			expect(URL).toEqual(`${process.env.ORIGIN}/search/chandler-az`)
 	})
 })
